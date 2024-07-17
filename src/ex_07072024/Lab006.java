d048a79a20b6c3231b4a348afd2c3693e94184e1
Dharini Thillainathan <dharinithillainathan@gmail.com>
2024-07-17 21:41:08 +0530

    All files added

--- src/ex_07072024/Lab006.java
+++ src/ex_07072024/Lab006.java
@@ -1,5 +1,8 @@
 package ex_07072024;
 
 public class Lab006 {
-
+    public static void main(String[] Dharini) {
+        System.out.println("Lab006");
+        System.out.println("Lab007");
+    }
 }
