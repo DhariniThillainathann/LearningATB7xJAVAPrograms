d28e2b08fdc47574340e79777531cd18c8a47bc1
Dharini Thillainathan <dharinithillainathan@gmail.com>
2024-07-18 10:59:38 +0530

    All files added

--- src/ex_07072024/Lab011.java
+++ src/ex_07072024/Lab011.java
@@ -4,5 +4,6 @@ public class Lab011 {
     public static void main(String args[]) {
         //instead of string[], string arg[] can be acceptable
         System.out.println("yes");
+        System.out.println("possible with changes of brace");
     }
 }
